640c30208f1fe5cc6aff3af7131bc5e6731f4dc8
Alivekeep <2fed.tcp@gmail.com>
2021-01-01 18:32:43 +0200

    refactor(web-version): - removed web examples - removed issue template - updated version

--- src/index.js
+++ src/index.js
@@ -3,24 +3,23 @@ const compile = require('./compile');
 const defaults = require('./defaults');
 
 /**
- * 模板引擎
- * @param   {string}            filename 模板名
- * @param   {Object|string}     content  数据或模板内容
- * @return  {string|function}            如果 content 为 string 则编译并缓存模板，否则渲染模板
+ * Template function
+ * @param   {string}            filename
+ * @param   {Object|string}     content
+ * @return  {string|function}
  */
-const template = (filename, content) => {
-    return content instanceof Object
-        ? render(
-              {
-                  filename
-              },
-              content
-          )
-        : compile({
-              filename,
-              source: content
-          });
-};
+const template = (filename, content) =>
+  content instanceof Object
+    ? render(
+        {
+          filename
+        },
+        content
+      )
+    : compile({
+        filename,
+        source: content
+      });
 
 template.render = render;
 template.compile = compile;
